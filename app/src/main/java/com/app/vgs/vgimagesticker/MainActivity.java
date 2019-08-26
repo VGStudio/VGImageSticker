34b166f660df0fb85bae0500b016a3d6a8c06177
vietgamestudio <vietgamestudio@gmail.com>
2019-08-26 13:49:02 +0700

    - update medium template of admob native ads

--- app/src/main/java/com/app/vgs/vgimagesticker/MainActivity.java
+++ app/src/main/java/com/app/vgs/vgimagesticker/MainActivity.java
@@ -1,10 +1,14 @@
 package com.app.vgs.vgimagesticker;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
+import com.app.vgs.vgimagesticker.utils.ScreenDimension;
+import com.google.android.ads.nativetemplates.NativeTemplateStyle;
 import com.google.android.ads.nativetemplates.TemplateView;
 import com.google.android.gms.ads.AdLoader;
 import com.google.android.gms.ads.AdRequest;
@@ -22,11 +26,22 @@ public class MainActivity extends AppCompatActivity {
 
 
         MobileAds.initialize(this, "ca-app-pub-3940256099942544~3347511713");
+
+        ScreenDimension.getScreenSize(this);
+
         loadNativeAd();
     }
 
     private void loadNativeAd(){
+        int screenWidth = ScreenDimension.mWidth;
+        int screenHeight = ScreenDimension.mHeight;
+
+
+
         template = findViewById(R.id.my_template);
+
+        //template.getLayoutParams().height = screenHeight/3;
+
         template.setVisibility(View.GONE);
         AdLoader.Builder builder = new AdLoader.Builder(
                 this, "ca-app-pub-3940256099942544/2247696110");
@@ -35,7 +50,13 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onUnifiedNativeAdLoaded(UnifiedNativeAd unifiedNativeAd) {
                 Log.d("VGImageSticker", "Load Native ad full");
+
+
+                ColorDrawable bgColor = new ColorDrawable(Color.WHITE);
+                NativeTemplateStyle styles = new
+                        NativeTemplateStyle.Builder().withMainBackgroundColor(bgColor).build();
                 template.setVisibility(View.VISIBLE);
+                //template.setStyles(styles);
                 template.setNativeAd(unifiedNativeAd);
             }
         });
---
+++ app/src/main/java/com/app/vgs/vgimagesticker/utils/ScreenDimension.java
@@ -0,0 +1,20 @@
+package com.app.vgs.vgimagesticker.utils;
+
+import android.app.Activity;
+import android.graphics.Point;
+import android.view.Display;
+
+public class ScreenDimension {
+
+    public static int mWidth;
+    public static int mHeight;
+
+    public static void getScreenSize(Activity activity){
+        Display display = activity.getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        mWidth = size.x;
+        mHeight = size.y;
+    }
+
+}
