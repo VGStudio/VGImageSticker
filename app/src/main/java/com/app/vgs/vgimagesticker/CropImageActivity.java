587b8d20cdf3fd76cd41835f4c7d040a2cf2435f
vietgamestudio <vietgamestudio@gmail.com>
2019-08-27 18:11:30 +0700

    - add cropper library

---
+++ app/src/main/java/com/app/vgs/vgimagesticker/CropImageActivity.java
@@ -0,0 +1,88 @@
+package com.app.vgs.vgimagesticker;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.view.View;
+
+import com.theartofdev.edmodo.cropper.CropImage;
+
+
+public class CropImageActivity extends BaseActivity {
+    public static final int REQUEST_IMAGE_FROM_GALLERY = 10001;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crop_image);
+    }
+
+    public void clickGallery(View view){
+        pickGallery();
+    }
+
+    public void pickGallery() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            requestPermission(Manifest.permission.READ_EXTERNAL_STORAGE,
+                    "",
+                    REQUEST_STORAGE_READ_ACCESS_PERMISSION);
+        } else {
+            Intent intent = new Intent(Intent.ACTION_GET_CONTENT)
+                    .setType("image/*")
+                    .addCategory(Intent.CATEGORY_OPENABLE);
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                String[] mimeTypes = {"image/jpeg", "image/png"};
+                intent.putExtra(Intent.EXTRA_MIME_TYPES, mimeTypes);
+            }
+
+            startActivityForResult(Intent.createChooser(intent, "Select picture"), REQUEST_IMAGE_FROM_GALLERY);
+        }
+    }
+
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_STORAGE_READ_ACCESS_PERMISSION:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    pickGallery();
+                }
+                break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK) {
+            if (requestCode == REQUEST_IMAGE_FROM_GALLERY) {
+                final Uri selectedUri = data.getData();
+                if (selectedUri != null) {
+                    startCrop(selectedUri);
+                } else {
+                    //Toast.makeText(this, R.string.toast_cannot_retrieve_selected_image, Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+
+    }
+
+    private void startCrop(@NonNull Uri uri) {
+        String destinationFileName = "img_crop_temp";
+        destinationFileName += ".png";
+        CropImage.activity(uri)
+                .start(this);
+
+
+    }
+}
