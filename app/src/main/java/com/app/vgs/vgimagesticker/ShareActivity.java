6b91ce0e1f4f5a0ab5268aad1217e35b4017137a
haidd <thongvantran45@gmail.com>
2019-09-12 15:30:15 +0700

    - Luan: Sửa lại cách code, tên biến, các hàm trong ShareActivity

--- app/src/main/java/com/app/vgs/vgimagesticker/ShareActivity.java
+++ app/src/main/java/com/app/vgs/vgimagesticker/ShareActivity.java
@@ -79,12 +79,13 @@ public class ShareActivity extends AppCompatActivity {
     DisplayMetrics displayMetrics;
     int width,height;
     Bitmap bitmap1,bitmap2;
+    String imgUrl;
+    Bitmap bitmap =null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_save_share);
-        StrictMode.ThreadPolicy policy = new   StrictMode.ThreadPolicy.Builder().permitAll().build();
-        StrictMode.setThreadPolicy(policy);
+        supportImage();
 
         initView();
         initData();
@@ -92,10 +93,6 @@ public class ShareActivity extends AppCompatActivity {
 
     private void initData() {
         mLstMoreAppGroup = JsonUtils.getMoreAppSaveJsonData(this, Const.MORE_APP_SAVE_DATA_FILE_PATH);
-        facebook();
-        instagram();
-        wallpaper();
-        share();
         addQuangCao();
         addMoreAppIconView();
     }
@@ -128,26 +125,17 @@ public class ShareActivity extends AppCompatActivity {
         }
     }
 
+
     // đưa ra dữ liệu của json
     private void fillDataMoreApp(){
         LayoutInflater layoutInflater = getLayoutInflater();
-        Bitmap bitmap =null;
-
         for (final MoreAppGroup moreAppGroup1 : mLstMoreAppGroup) {
             final View view = layoutInflater.inflate(R.layout.layout_sub_btn_moreapp, mGrid, false);
             ImageView imgButton = view.findViewById(R.id.imgMoreApp);
             TextView textView = view.findViewById(R.id.txtMoreApp);
             textView.setText(moreAppGroup1.getTittle());
-            String imgUrl = moreAppGroup1.getIcon();
-            try {
-                URL url = new URL(imgUrl);
-                HttpURLConnection httpConn = (HttpURLConnection) url.openConnection();
-                httpConn.connect();
-                InputStream in = httpConn.getInputStream();
-                bitmap = BitmapFactory.decodeStream(in);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+            imgUrl = moreAppGroup1.getIcon();
+            readDataImageInternet();
             imgButton.setImageBitmap(bitmap);
 
             imgButton.setOnClickListener(new View.OnClickListener() {
@@ -167,7 +155,24 @@ public class ShareActivity extends AppCompatActivity {
             mGrid.addView(view);
         }
     }
-    //
+    // đọc dữ liệu hình ảnh trên mạng
+    public void readDataImageInternet(){
+        try {
+            URL url = new URL(imgUrl);
+            HttpURLConnection httpConn = (HttpURLConnection) url.openConnection();
+            httpConn.connect();
+            InputStream in = httpConn.getInputStream();
+            bitmap = BitmapFactory.decodeStream(in);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    //support fill data Image for Internet
+    public void supportImage(){
+        StrictMode.ThreadPolicy policy = new   StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+    }
+    /////////////////////
 
     // thanh quảng cáo ở màn hình
     private void addQuangCao(){
@@ -190,53 +195,56 @@ public class ShareActivity extends AppCompatActivity {
         AdLoader adLoader = builder.build();
         adLoader.loadAd(new AdRequest.Builder().build());
     }
+    //
+    ////////////////////////////////
 
+
+    public Intent intent;
     //click vào facebook
     @SuppressLint("WrongConstant")
-    public void facebook(){
-        mFacebook.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+    public void facebook(View view){
                 if (!isApplicationSentToBackground(getApplicationContext()))
                 {
-                    Intent paramView;
-                    paramView = new Intent("android.intent.action.SEND");
-                    paramView.setType("text/plain");
-                    paramView.putExtra("android.intent.extra.TITLE", "Smarty App");
-                    paramView.putExtra("android.intent.extra.TEXT", "Created using Smarty App. Use following install link to download :- https://play.google.com/store/apps/details?id=com.appwallet.smarty");
-                    paramView.putExtra("android.intent.extra.SUBJECT", "Smarty App");
-                    Object localObject1 = new StrictMode.VmPolicy.Builder();
-                    StrictMode.setVmPolicy(((StrictMode.VmPolicy.Builder)localObject1).build());
-                    if (Build.VERSION.SDK_INT >= 18) {
-                        ((StrictMode.VmPolicy.Builder)localObject1).detectFileUriExposure();
-                    }
-                    try {
-                        localObject1 = Uri.parse(getIntent().getStringExtra("imageToShare-uri"));
-                        paramView.setType("image/png");
-                        paramView.putExtra("android.intent.extra.STREAM", (Parcelable) localObject1);
-                        localObject1 = getApplicationContext().getPackageManager().queryIntentActivities(paramView, 0).iterator();
-                    }catch (Exception e){
-                        e.printStackTrace();
-                    }
-                    while (((Iterator)localObject1).hasNext())
-                    {
-                        Object localObject2 = (ResolveInfo)((Iterator)localObject1).next();
-                        if (((ResolveInfo)localObject2).activityInfo.name.contains("facebook"))
-                        {
-                            localObject2 = ((ResolveInfo)localObject2).activityInfo;
-                            localObject2 = new ComponentName(((ActivityInfo)localObject2).applicationInfo.packageName, ((ActivityInfo)localObject2).name);
-                            paramView.addCategory("android.intent.category.LAUNCHER");
-                            paramView.setFlags(270532608);
-                            paramView.setComponent((ComponentName)localObject2);
-                            startActivity(paramView);
-                        }
-                    }
+                    intent = new Intent("android.intent.action.SEND");
+                    intent.setType("text/plain");
+                    intent.putExtra("android.intent.extra.TITLE", "Smarty App");
+                    intent.putExtra("android.intent.extra.TEXT", "Created using Smarty App. Use following install link to download :- https://play.google.com/store/apps/details?id=com.appwallet.smarty");
+                    intent.putExtra("android.intent.extra.SUBJECT", "Smarty App");
+                    supportDataFacebook();
                 }
-            }
-        });
-
-
     }
+
+    // tìm địa chỉ tới nguồn ảnh để liên kết facebook
+    @SuppressLint("WrongConstant")
+    public void supportDataFacebook(){
+        Object uriObj1 = new StrictMode.VmPolicy.Builder();
+        StrictMode.setVmPolicy(((StrictMode.VmPolicy.Builder)uriObj1).build());
+        if (Build.VERSION.SDK_INT >= 18) {
+            ((StrictMode.VmPolicy.Builder)uriObj1).detectFileUriExposure();
+        }
+        try {
+            uriObj1 = Uri.parse(getIntent().getStringExtra("imageToShare-uri"));
+            intent.setType("image/png");
+            intent.putExtra("android.intent.extra.STREAM", (Parcelable) uriObj1);
+            uriObj1 = getApplicationContext().getPackageManager().queryIntentActivities(intent, 0).iterator();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        while (((Iterator)uriObj1).hasNext())
+        {
+            Object uriObj2 = (ResolveInfo)((Iterator)uriObj1).next();
+            if (((ResolveInfo)uriObj2).activityInfo.name.contains("facebook"))
+            {
+                uriObj2 = ((ResolveInfo)uriObj2).activityInfo;
+                uriObj2 = new ComponentName(((ActivityInfo)uriObj2).applicationInfo.packageName, ((ActivityInfo)uriObj2).name);
+                intent.addCategory("android.intent.category.LAUNCHER");
+                intent.setFlags(270532608);
+                intent.setComponent((ComponentName)uriObj2);
+                startActivity(intent);
+            }
+        }
+    }
+
     //hỗ trợ facebook
     public boolean isApplicationSentToBackground(Context paramContext)
     {
@@ -244,23 +252,64 @@ public class ShareActivity extends AppCompatActivity {
         return (!localList.isEmpty()) && (!((ActivityManager.RunningTaskInfo)localList.get(0)).topActivity.getPackageName().equals(paramContext.getPackageName()));
     }
     //
+    ///////////////////
+
 
     // click vào istagram
-    private void instagram(){
-        mInstagram.setOnClickListener(new View.OnClickListener() {
-            @SuppressLint("WrongConstant")
-            @Override
-            public void onClick(View view) {
-                shareInstagram(mFileImagePath);
-            }
-        });
+    public void instagram(View view){
+        checkAppInstall();
+        shareInstagram(mFileImagePath);
     }
 
+    //test instagram
+    public boolean checkAppInstall() {
+        String uri = null;
+        PackageManager pm = getPackageManager();
+        try {
+            pm.getPackageInfo(uri, PackageManager.GET_ACTIVITIES);
+            return true;
+        } catch (PackageManager.NameNotFoundException e) {
+            //Error
+        }
+
+        return false;
+    }
+
+    public void shareInstagram(File mFileImagePath) {
+        try {
+            Intent intent = getPackageManager().getLaunchIntentForPackage("com.instagram.android");
+            if (intent != null) {
+                intentInstagram();
+            } else {
+                Toast.makeText(ShareActivity.this, "Instagram have not been installed.", Toast.LENGTH_SHORT).show();
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+    //chuyển địa chỉ tới instagram
+    public void intentInstagram(){
+        Intent mIntentShare = new Intent(Intent.ACTION_SEND);
+        String mStrExtension = android.webkit.MimeTypeMap.getFileExtensionFromUrl(Uri.fromFile(mFileImagePath).toString());
+        String mStrMimeType = android.webkit.MimeTypeMap.getSingleton().getMimeTypeFromExtension(mStrExtension);
+        if (mStrExtension.equalsIgnoreCase("") || mStrMimeType == null) {
+            // if there is no extension or there is no definite mimetype, still try to open the file
+            mIntentShare.setType("text*//*");
+        } else {
+            mIntentShare.setType(mStrMimeType);
+        }
+        mIntentShare.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(mFileImagePath));
+        mIntentShare.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+
+        mIntentShare.setPackage("com.instagram.android");
+        startActivity(mIntentShare);
+    }
+    //
+    /////////////////////
+
+
     // click vào wall paper
-    private void wallpaper(){
-        mWallpaper.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+    public void wallpaper(View view){
                 GetScreenWidthHeight();
                 SetBitmapSize();
                 wallpaperManager = WallpaperManager.getInstance(ShareActivity.this);
@@ -271,36 +320,7 @@ public class ShareActivity extends AppCompatActivity {
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
-            }
-        });
     }
-
-    // click  vào share
-    private void share(){
-        mShare.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent share = new Intent(Intent.ACTION_SEND);
-
-                // If you want to share a png image only, you can do:
-                // setType("image/png"); OR for jpeg: setType("image/jpeg");
-                share.setType("image/*");
-
-                // Make sure you put example png image named myImage.png in your
-                // directory
-                String imagePath = Environment.getExternalStorageDirectory()
-                        + "/myImage.png";
-
-                File imageFileToShare = new File(imagePath);
-
-                Uri uri = Uri.fromFile(imageFileToShare);
-                share.putExtra(Intent.EXTRA_STREAM, uri);
-
-                startActivity(Intent.createChooser(share, "Share Image!"));
-            }
-        });
-    }
-
     // hỗ trợ cho hình nền wallpaper
     public void GetScreenWidthHeight(){
 
@@ -320,43 +340,24 @@ public class ShareActivity extends AppCompatActivity {
 
     }
     //
-
-    //test instagram
-
-    private boolean checkAppInstall(String uri) {
-        PackageManager pm = getPackageManager();
-        try {
-            pm.getPackageInfo(uri, PackageManager.GET_ACTIVITIES);
-            return true;
-        } catch (PackageManager.NameNotFoundException e) {
-            //Error
-        }
-
-        return false;
-    }
+    /////////////////////
 
 
-    private void shareInstagram(File mFileImagePath) {
-        Intent intent = getPackageManager().getLaunchIntentForPackage("com.instagram.android");
-        if (intent != null) {
-            Intent mIntentShare = new Intent(Intent.ACTION_SEND);
-            String mStrExtension = android.webkit.MimeTypeMap.getFileExtensionFromUrl(Uri.fromFile(mFileImagePath).toString());
-            String mStrMimeType = android.webkit.MimeTypeMap.getSingleton().getMimeTypeFromExtension(mStrExtension);
-            if (mStrExtension.equalsIgnoreCase("") || mStrMimeType == null) {
-                // if there is no extension or there is no definite mimetype, still try to open the file
-                mIntentShare.setType("text*//*");
-            } else {
-                mIntentShare.setType(mStrMimeType);
-            }
-            mIntentShare.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(mFileImagePath));
-            mIntentShare.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
-
-            mIntentShare.setPackage("com.instagram.android");
-            startActivity(mIntentShare);
-        } else {
-            Toast.makeText(ShareActivity.this, "Instagram have not been installed.", Toast.LENGTH_SHORT).show();
-        }
+    // click  vào share
+    public void share(View views){
+                Intent share = new Intent(Intent.ACTION_SEND);
+                // If you want to share a png image only, you can do:
+                // setType("image/png"); OR for jpeg: setType("image/jpeg");
+                share.setType("image/*");
+                // Make sure you put example png image named myImage.png in your
+                // directory
+                String imagePath = Environment.getExternalStorageDirectory()
+                        + "/myImage.png";
+                File imageFileToShare = new File(imagePath);
+                Uri uri = Uri.fromFile(imageFileToShare);
+                share.putExtra(Intent.EXTRA_STREAM, uri);
+                startActivity(Intent.createChooser(share, "Share Image!"));
     }
     //
-
+    ////////////////////////////
 }
